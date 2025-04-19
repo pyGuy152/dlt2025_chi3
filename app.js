006028ebfc0264d5635ac4803d931a5c3de93263
pyGuy152 <sarveshsabe@gmail.com>
2025-04-18 20:17:08 -0500

    added about and contact page

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
